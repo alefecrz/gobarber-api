17f8ac3d7642760fcce9d43d446d727d792384a5
Álefe Cruz <alefecrz@gmail.com>
2020-03-17 17:31:56 -0300

    Cadastrar usuário.

--- src/router.js
+++ src/router.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Alefe Cruz',
-    email: 'alefecrz@gmail.com',
-    password_hash: '12e12323',
-  });
-  return res.send(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
