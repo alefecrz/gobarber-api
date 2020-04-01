6ebc3572af9c5d35375dad703d1522a60d5ea633
Álefe Cruz <alefecrz@gmail.com>
2020-04-01 12:47:28 -0300

    Upload image with multer.

--- src/router.js
+++ src/router.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -6,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,5 +16,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
